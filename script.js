eba801959265629492622b2f37765b0f6abf4e63
Shubhankar Das <shubhankar@browserstack.com>
2020-07-15 00:19:32 +0530

    0.0.2 extension

---
+++ script.js
@@ -0,0 +1,26 @@
+var BASE_URL = 'https://have-a-happy-day.web.app/';
+var xhr = new XMLHttpRequest();
+xhr.open('GET', BASE_URL + 'quote');
+xhr.send(null);
+
+xhr.onreadystatechange = function () {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK) {
+      var response = JSON.parse(xhr.responseText);
+      renderQuote(response);
+    } else {
+      console.log('Error: ' + xhr.status); // An error occurred during the request.
+    }
+    xhr.onreadystatechange = null;
+  }
+};
+
+function renderQuote(response) {
+  document.getElementById('message').innerHTML = response.res.message;
+  document.getElementById('image').src =
+    BASE_URL + 'images/' + response.res.image_name;
+  document.getElementById('default').classList.add('fade-out');
+  document.getElementsByClassName('container')[0].classList.add('fade-in');
+}
