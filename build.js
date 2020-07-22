2c8b907e03294c6b0ad28d25ee7d2b992f96bb1f
Shubhankar Das <shubhankar@browserstack.com>
2020-07-22 21:37:47 +0530

    0.0.0.4

---
+++ build.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const archiver = require('archiver');
+const version = require('./manifest.json')['version'];
+
+const archive = archiver('zip');
+const output = fs.createWriteStream(
+  `${__dirname}/builds/${Date.now()}_${version}.zip`
+);
+
+const filesToAdd = [
+  {
+    path: `${__dirname}/logo-19.png`,
+    name: 'logo-19.png',
+  },
+  {
+    path: `${__dirname}/logo-38.png`,
+    name: 'logo-39.png',
+  },
+  {
+    path: `${__dirname}/logo-48.png`,
+    name: 'logo-48.png',
+  },
+  {
+    path: `${__dirname}/logo-90.png`,
+    name: 'logo-90.png',
+  },
+  {
+    path: `${__dirname}/logo-128.png`,
+    name: 'logo-128.png',
+  },
+  {
+    path: `${__dirname}/index.html`,
+    name: 'index.html',
+  },
+  {
+    path: `${__dirname}/manifest.json`,
+    name: 'manifest.json',
+  },
+  {
+    path: `${__dirname}/script.js`,
+    name: 'script.js',
+  },
+  {
+    path: `${__dirname}/styles.css`,
+    name: 'styles.css',
+  },
+  {
+    path: `${__dirname}/background.js`,
+    name: 'background.js',
+  },
+];
+
+output.on('close', function () {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('Build is ready');
+});
+
+archive.on('error', function (err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+filesToAdd.forEach((file) => {
+  archive.append(fs.createReadStream(file.path), { name: file.name });
+});
+
+archive.finalize();
