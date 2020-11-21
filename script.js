166e46cb9d28f73075e509881dbff6044e435b79
Shubhankar Das <shubh065@gmail.com>
2020-11-22 01:33:48 +0530

    updated image links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var BASE_URL = 'https://have-a-happy-day.web.app/';
+var BASE_URL = "https://have-a-happy-day.web.app/";
 var xhr = new XMLHttpRequest();
-xhr.open('GET', BASE_URL + 'quote');
+xhr.open("GET", BASE_URL + "quote");
 xhr.send(null);
 
 xhr.onreadystatechange = function () {
@@ -11,16 +11,21 @@ xhr.onreadystatechange = function () {
       var response = JSON.parse(xhr.responseText);
       renderQuote(response);
     } else {
-      console.log('Error: ' + xhr.status); // An error occurred during the request.
+      console.log("Error: " + xhr.status); // An error occurred during the request.
     }
     xhr.onreadystatechange = null;
   }
 };
 
 function renderQuote(response) {
-  document.getElementById('message').innerHTML = response.res.message;
-  document.getElementById('image').src =
-    BASE_URL + 'images/' + response.res.image_name;
-  document.getElementById('default').classList.add('fade-out');
-  document.getElementsByClassName('container')[0].classList.add('fade-in');
+  document.getElementById("message").innerHTML = response.res.message;
+  let imageUrl = "";
+  if (response.res.image_link) {
+    imageUrl = image_link;
+  } else {
+    imageUrl = BASE_URL + "images/" + response.res.image_name;
+  }
+  document.getElementById("image").src = imageUrl;
+  document.getElementById("default").classList.add("fade-out");
+  document.getElementsByClassName("container")[0].classList.add("fade-in");
 }
