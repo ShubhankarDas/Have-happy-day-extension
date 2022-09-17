4bc3af3cc8fb5ab5c171688f89f7ae4586d75c78
shubhankar <shubh065@gmail.com>
2022-09-17 19:04:35 +0530

    added theme option

--- build.js
+++ build.js
@@ -1,57 +1,56 @@
-const fs = require('fs');
-const archiver = require('archiver');
-const version = require('./manifest.json')['version'];
+const fs = require("fs");
+const archiver = require("archiver");
+const version = require("./manifest.json")["version"];
 
-const archive = archiver('zip');
-const output = fs.createWriteStream(
-  `${__dirname}/builds/${Date.now()}_${version}.zip`
-);
+const archive = archiver("zip");
+const filename = `${Date.now()}_${version}.zip`;
+const output = fs.createWriteStream(`${__dirname}/builds/${filename}`);
 
 const filesToAdd = [
   {
     path: `${__dirname}/logo-19.png`,
-    name: 'logo-19.png',
+    name: "logo-19.png",
   },
   {
     path: `${__dirname}/logo-38.png`,
-    name: 'logo-38.png',
+    name: "logo-38.png",
   },
   {
     path: `${__dirname}/logo-48.png`,
-    name: 'logo-48.png',
+    name: "logo-48.png",
   },
   {
     path: `${__dirname}/logo-90.png`,
-    name: 'logo-90.png',
+    name: "logo-90.png",
   },
   {
     path: `${__dirname}/logo-128.png`,
-    name: 'logo-128.png',
+    name: "logo-128.png",
   },
   {
     path: `${__dirname}/index.html`,
-    name: 'index.html',
+    name: "index.html",
   },
   {
     path: `${__dirname}/manifest.json`,
-    name: 'manifest.json',
+    name: "manifest.json",
   },
   {
     path: `${__dirname}/script.js`,
-    name: 'script.js',
+    name: "script.js",
   },
   {
     path: `${__dirname}/styles.css`,
-    name: 'styles.css',
+    name: "styles.css",
   },
 ];
 
-output.on('close', function () {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('Build is ready');
+output.on("close", function () {
+  console.log(archive.pointer() + " total bytes");
+  console.log(`Build is ready - ${filename}`);
 });
 
-archive.on('error', function (err) {
+archive.on("error", function (err) {
   throw err;
 });
 
--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 var BASE_URL = "https://have-a-happy-day.web.app/";
 var xhr = new XMLHttpRequest();
-xhr.open("GET", BASE_URL + "quote");
-xhr.send(null);
 
-xhr.onreadystatechange = function () {
-  var DONE = 4; // readyState 4 means the request is done.
-  var OK = 200; // status 200 is a successful return.
-  if (xhr.readyState === DONE) {
-    if (xhr.status === OK) {
-      var response = JSON.parse(xhr.responseText);
-      renderQuote(response);
-    } else {
-      console.log("Error: " + xhr.status); // An error occurred during the request.
-    }
-    xhr.onreadystatechange = null;
+var ThemeManager = (function () {
+  function _checkForLocalStorage() {
+    return !!window.localStorage;
   }
-};
+
+  function _setLocalStorage(key, val) {
+    if (!_checkForLocalStorage) {
+      return;
+    }
+    return localStorage.setItem(key, val);
+  }
+
+  function _getLocalStorage(key) {
+    if (!_checkForLocalStorage) {
+      return;
+    }
+    return localStorage.getItem(key);
+  }
+
+  function changeTheme(mode) {
+    try {
+      if (mode) {
+        document.querySelector("body").className = mode;
+        _setLocalStorage("theme-option", mode);
+      }
+    } catch (e) {}
+  }
+
+  function setListener() {
+    document
+      .querySelector(".options-container")
+      .addEventListener("click", function (e) {
+        var currentColorMode = document.querySelector("body").className;
+        if (currentColorMode === "dark-mode") {
+          changeTheme("light-mode");
+        } else {
+          changeTheme("dark-mode");
+        }
+      });
+  }
+
+  function init() {
+    if (_getLocalStorage("theme-option")) {
+      changeTheme(_getLocalStorage("theme-option"));
+    } else {
+      changeTheme("light-mode");
+    }
+    setListener();
+  }
+
+  return {
+    init: init,
+    changeTheme: changeTheme,
+    setListener: setListener,
+  };
+})();
 
 function renderQuote(response) {
   document.getElementById("message").innerHTML = response.res.message;
@@ -29,3 +70,21 @@ function renderQuote(response) {
   document.getElementById("default").classList.add("fade-out");
   document.getElementsByClassName("container")[0].classList.add("fade-in");
 }
+
+ThemeManager.init();
+
+xhr.open("GET", BASE_URL + "quote");
+xhr.send(null);
+xhr.onreadystatechange = function () {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK) {
+      var response = JSON.parse(xhr.responseText);
+      renderQuote(response);
+    } else {
+      console.log("Error: " + xhr.status); // An error occurred during the request.
+    }
+    xhr.onreadystatechange = null;
+  }
+};
